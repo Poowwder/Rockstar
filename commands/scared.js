1ed00a1e0e45c26522c28ac3d046ba5b3e7edcbe
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:21:11 -0300

    Update scared.js

--- commands/scared.js
+++ commands/scared.js
@@ -1,12 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'scared',
-    description: '¡Muestra que tienes mucho miedo! 😨',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setDescription(`😨 **${message.author.username}** está muy asustada... ¡Alguien que la cuide! 🌸`)
-            .setImage("https://media.tenor.com/7P7S9X9v_YAAAAC/anime-scared.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'scared', description: 'Tienes miedo 😨', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'scared');
+        await input.reply(result);
     }
 };
