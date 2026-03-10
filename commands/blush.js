d63029708eb26bfbf9e02d5484cb91e094c46d14
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:01:46 -0300

    Update blush.js

--- commands/blush.js
+++ commands/blush.js
@@ -1,15 +1,11 @@
-const { EmbedBuilder } = require('discord.js');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const { runReaction } = require('../utils/reactionHandler.js');
 
 module.exports = {
     name: 'blush',
-    description: 'Muéstrate sonrojada y cute 😳',
-    async execute(message) {
-        const res = await fetch('https://nekos.life/api/v2/img/smug'); // Smug/Blush estilo anime
-        const json = await res.json();
-        const embed = new EmbedBuilder()
-            .setDescription(`😳 **${message.author.username}** se ha sonrojado mucho... ¡Qué linda te ves! ✨`)
-            .setImage(json.url).setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    description: 'Siente el rubor en tus mejillas 😊',
+    category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'blush');
+        await input.reply(result);
     }
 };
