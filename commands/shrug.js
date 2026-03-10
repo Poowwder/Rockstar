bc8731a32fba03c7e1541f0f05224b26200f3884
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:21:45 -0300

    Update shrug.js

--- commands/shrug.js
+++ commands/shrug.js
@@ -1,12 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'shrug',
-    description: 'No tienes idea de qué está pasando 🤷‍♀️',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setDescription(`🤷‍♀️ **${message.author.username}** no tiene idea... ¿Quién sabe? ✨`)
-            .setImage("https://media.tenor.com/f7jYn_Ush88AAAAC/anime-shrug.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'shrug', description: 'Encógete de hombros 🤷‍♀️', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'shrug');
+        await input.reply(result);
     }
 };
