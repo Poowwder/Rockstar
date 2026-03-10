0a4323b8f5286e8888d270af386915280e6e7719
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:16:29 -0300

    Update grafitti.js

--- commands/grafitti.js
+++ commands/grafitti.js
@@ -1,20 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'graffiti',
-    aliases: ['paint', 'esprintar'],
-    description: '¡Deja tu marca con un graffiti aesthetic! 🎨',
-    async execute(message, args) {
-        const user = message.mentions.users.first();
-        const desc = user 
-            ? `🎨 **${message.author.username}** llenó de pintura a **${user.username}**... ¡Eres su nuevo graffiti favorito! ✨` 
-            : `🎨 **${message.author.username}** sacó sus latas de pintura y pintó un graffiti de Rockstar increíble. 🌸`;
-
-        const embed = new EmbedBuilder()
-            .setDescription(desc)
-            .setImage("https://media.tenor.com/6_GfN_lX9v8AAAAC/anime-graffiti.gif")
-            .setColor('#FFB7C5');
-
-        await message.reply({ embeds: [embed] });
+    name: 'grafitti', description: 'Haz un graffiti 🎨', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'grafitti');
+        await input.reply(result);
     }
 };
