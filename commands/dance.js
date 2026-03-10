261c0f2375eaacd445038d76549c6f72b61f9766
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:02:58 -0300

    Update dance.js

--- commands/dance.js
+++ commands/dance.js
@@ -1,12 +1,11 @@
-const { EmbedBuilder } = require('discord.js');
+const { runReaction } = require('../utils/reactionHandler.js');
 
 module.exports = {
     name: 'dance',
-    description: '¡Ponte a bailar con estilo! 💃',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setDescription(`💃 **${message.author.username}** sacó sus mejores pasos... ¡Esa es mi reina! ✨`)
-            .setImage("https://media.tenor.com/u_f669S8X78AAAAC/anime-dance.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    description: 'Déjate llevar por el ritmo 💃',
+    category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'dance');
+        await input.reply(result);
     }
 };
