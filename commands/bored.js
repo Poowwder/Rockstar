38e7320144fe1fb2d91f9bade5b14c11d2864619
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:14:04 -0300

    Update bored.js

--- commands/bored.js
+++ commands/bored.js
@@ -1,12 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'bored',
-    description: 'Muestra que estás muy aburrida 😴',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setDescription(`😴 **${message.author.username}** se está muriendo de aburrimiento... ✨`)
-            .setImage("https://media.tenor.com/8-zI5S_5eIAAAAAC/anime-bored.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'bored', description: 'Estás aburrida 😑', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'bored');
+        await input.reply(result);
     }
 };
