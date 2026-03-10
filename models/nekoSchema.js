d6f1de9bb8446c60986245c2bf842f75f888616a
Poowwder <ibettyboopp@gmail.com>
2026-03-10 15:46:09 -0300

    Update nekoSchema.js

--- models/nekoSchema.js
+++ models/nekoSchema.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const nekoSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     guildId: { type: String, required: true },
-    activityPoints: { type: Number, default: 0 }, // Puntos por mensajes
-    actionPoints: { type: Number, default: 0 },   // Puntos por comandos de acción (hug, kiss)
+    activityPoints: { type: Number, default: 0 }, 
+    actionPoints: { type: Number, default: 0 },   
     level: { type: Number, default: 1 },
+    unlockedNekos: { type: Array, default: [] }, // <-- AQUÍ se guardan los gatitos (Badges)
     lastMessage: { type: Date, default: Date.now }
 });
 
-// Esto evita que se creen duplicados si el usuario ya existe
 nekoSchema.index({ userId: 1, guildId: 1 }, { unique: true });
 
 module.exports = mongoose.model('Neko', nekoSchema);
