62bbcd916c6d0d93f640fc381fa2157ffdfc240f
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:18:55 -0300

    Update paint.js

--- commands/paint.js
+++ commands/paint.js
@@ -1,18 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'paint',
-    description: '¡Pinta un cuadro o a un amigo! 🖌️',
-    async execute(message, args) {
-        const user = message.mentions.users.first();
-        const desc = user 
-            ? `🖌️ **${message.author.username}** está usando a **${user.username}** como lienzo... ¡Qué colorido! ✨` 
-            : `🖌️ **${message.author.username}** se puso a pintar un cuadro súper aesthetic. 🌸`;
-
-        const embed = new EmbedBuilder()
-            .setDescription(desc)
-            .setImage("https://media.tenor.com/xscbS6o9Ym8AAAAC/anime-painting.gif")
-            .setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'paint', description: 'Ponte a pintar 🖌️', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'paint');
+        await input.reply(result);
     }
 };
