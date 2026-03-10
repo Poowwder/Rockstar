490a1ffa77f94186db6b3ea9692222d8b5332966
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:07:45 -0300

    Update lick.js

--- commands/lick.js
+++ commands/lick.js
@@ -1,17 +1,10 @@
-const { EmbedBuilder } = require('discord.js');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
+const { runAction } = require('../utils/actionHandler.js');
 module.exports = {
-    name: 'lick',
-    description: 'Dale una lamiidita a alguien... ¡Qué atrevida! 👅',
-    async execute(message, args) {
-        const user = message.mentions.users.first();
-        if (!user) return message.reply("🌸 ¿A quién quieres lamer, linda? ✨");
-        const res = await fetch('https://nekos.life/api/v2/img/lick');
-        const json = await res.json();
-        const embed = new EmbedBuilder()
-            .setDescription(`👅 **${message.author.username}** le dio una lamida a **${user.username}**... ✨`)
-            .setImage(json.url).setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'lick', description: 'Lame a alguien', category: 'interacción',
+    async execute(input) {
+        const target = input.mentions.users.first();
+        if (!target) return input.reply({ content: "❌ Las sombras exigen que menciones a un objetivo.", ephemeral: true });
+        const result = await runAction(input, 'lick', target);
+        await input.reply(result);
     }
 };
