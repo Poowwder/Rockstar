6072689d8c149e15ee597c2727179f7d195da9c9
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:17:51 -0300

    Update laugh.js

--- commands/laugh.js
+++ commands/laugh.js
@@ -1,12 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'laugh',
-    description: '¡Ríete a carcajadas! ✨',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setDescription(`✨ **${message.author.username}** no puede parar de reír... ¡Qué alegría! 🌸`)
-            .setImage("https://media.tenor.com/T_7kM_88v7MAAAAC/anime-laugh.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'laugh', description: 'Ríete un poco 😂', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'laugh');
+        await input.reply(result);
     }
 };
