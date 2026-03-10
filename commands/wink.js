84a989a2a909ec1a7de5e9633a93cffb95a58a0e
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:24:38 -0300

    Update wink.js

--- commands/wink.js
+++ commands/wink.js
@@ -1,14 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'wink',
-    description: 'Guiñale el ojo a alguien con estilo 😉',
-    async execute(message, args) {
-        const user = message.mentions.users.first();
-        const desc = user ? `😉 **${message.author.username}** le guiñó el ojo a **${user.username}**... ¡Qué coqueta! ✨` : `😉 **${message.author.username}** lanzó un guiño... ✨`;
-        const embed = new EmbedBuilder()
-            .setDescription(desc)
-            .setImage("https://media.tenor.com/vPvPvPvPvPyAAAAC/anime-wink.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'wink', description: 'Guiña un ojo 😉', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'wink');
+        await input.reply(result);
     }
 };
