43b8b0178a606c6bea1c003e0478f82430fa1e7b
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:20:33 -0300

    Update run.js

--- commands/run.js
+++ commands/run.js
@@ -1,12 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'run',
-    description: '¡Huye de la situación! 🏃‍♀️',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setDescription(`🏃‍♀️ **${message.author.username}** salió corriendo... ¡No la atraparán! ✨`)
-            .setImage("https://media.tenor.com/N6N6N6N6N6NAAAAC/anime-run.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'run', description: '¡Corre! 🏃‍♀️', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'run');
+        await input.reply(result);
     }
 };
