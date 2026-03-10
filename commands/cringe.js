49ffeacbc22de92166383035dfd8a6ecc2f6ce03
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:15:23 -0300

    Update cringe.js

--- commands/cringe.js
+++ commands/cringe.js
@@ -1,12 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'cringe',
-    description: 'Cuando alguien dice algo que da mucha vergüenza... 😬',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setDescription(`😬 **${message.author.username}** sintió un poquito de cringe... ✨`)
-            .setImage("https://media.tenor.com/7p6p6p6p6p6AAAAC/anime-cringe.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'cringe', description: '¡Qué cringe! 😬', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'cringe');
+        await input.reply(result);
     }
 };
