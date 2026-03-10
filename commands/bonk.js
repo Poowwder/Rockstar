fd5575152482e0d202329754d40b4df82f1a3eba
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:13:44 -0300

    Update bonk.js

--- commands/bonk.js
+++ commands/bonk.js
@@ -1,13 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'bonk',
-    description: '¡Dale un martillazo a alguien! 🔨',
-    async execute(message, args) {
-        const user = message.mentions.users.first();
-        if (!user) return message.reply("🌸 ¿A quién vas a mandarle un bonk, linda? ✨");
-        const embed = new EmbedBuilder()
-            .setDescription(`🔨 **${message.author.username}** le dio un BONK a **${user.username}**... ¡Directo a la horny jail! 🎀`)
-            .setImage("https://media.tenor.com/7161m1vP-o8AAAAC/anime-bonk.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'boonk', description: '¡Boonk! 💥', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'boonk');
+        await input.reply(result);
     }
 };
