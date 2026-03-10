8de6af50aeb667a4fb312c52a5e2b4fef6935c78
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:08:39 -0300

    Update poke.js

--- commands/poke.js
+++ commands/poke.js
@@ -1,17 +1,10 @@
-const { EmbedBuilder } = require('discord.js');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
+const { runAction } = require('../utils/actionHandler.js');
 module.exports = {
-    name: 'poke',
-    description: 'Pica a alguien con el dedo para llamar su atención 👉',
-    async execute(message, args) {
-        const user = message.mentions.users.first();
-        if (!user) return message.reply("🌸 Tienes que picar a alguien, reina. ✨");
-        const res = await fetch('https://nekos.life/api/v2/img/poke');
-        const json = await res.json();
-        const embed = new EmbedBuilder()
-            .setDescription(`👉 **${message.author.username}** está picando a **${user.username}**... ¡Hey, hazle caso! 🌸`)
-            .setImage(json.url).setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'poke', description: 'Pica a alguien', category: 'interacción',
+    async execute(input) {
+        const target = input.mentions.users.first();
+        if (!target) return input.reply({ content: "❌ Las sombras exigen que menciones a un objetivo.", ephemeral: true });
+        const result = await runAction(input, 'poke', target);
+        await input.reply(result);
     }
 };
