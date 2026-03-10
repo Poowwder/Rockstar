34ff54850860d2dc7e9183755a6e0bbc2f5fca1e
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:22:21 -0300

    Update sip.js

--- commands/sip.js
+++ commands/sip.js
@@ -1,12 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'sip',
-    description: 'Toma un traguito de tu bebida favorita ☕',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setDescription(`☕ **${message.author.username}** está bebiendo algo... ¡Salud, reina! ✨`)
-            .setImage("https://media.tenor.com/Y1Y2Y3Y4Y5YAAAAC/anime-sip.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'sip', description: 'Bebe algo 🥤', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'sip');
+        await input.reply(result);
     }
 };
