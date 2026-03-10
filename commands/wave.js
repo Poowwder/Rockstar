f25a193110f013893841d347b366e535f219806b
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:24:14 -0300

    Update wave.js

--- commands/wave.js
+++ commands/wave.js
@@ -1,14 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'wave',
-    description: 'Saluda a alguien con la mano 👋',
-    async execute(message, args) {
-        const user = message.mentions.users.first();
-        const desc = user ? `👋 **${message.author.username}** está saludando a **${user.username}**... ¡Hola, hola! ✨` : `👋 **${message.author.username}** está saludando a todos... ¡Qué linda! ✨`;
-        const embed = new EmbedBuilder()
-            .setDescription(desc)
-            .setImage("https://media.tenor.com/mS2Q-2u5f_YAAAAC/anime-wave.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'wave', description: 'Saluda con la mano 👋', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'wave');
+        await input.reply(result);
     }
 };
