9b7241441228de345d113608b0cf2d355399e8e4
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:11:03 -0300

    Update splash.js

--- commands/splash.js
+++ commands/splash.js
@@ -1,10 +1,10 @@
 const { runAction } = require('../utils/actionHandler.js');
 module.exports = {
-    name: 'spray', description: 'Rocía a alguien', category: 'interacción',
+    name: 'splash', description: 'Salpica a alguien', category: 'interacción',
     async execute(input) {
         const target = input.mentions.users.first();
         if (!target) return input.reply({ content: "❌ Las sombras exigen que menciones a un objetivo.", ephemeral: true });
-        const result = await runAction(input, 'spray', target);
+        const result = await runAction(input, 'splash', target);
         await input.reply(result);
     }
 };
