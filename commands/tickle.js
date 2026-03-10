f2d30d3a4d9959cdb69d47a276a08312a1cebae8
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:12:57 -0300

    Update tickle.js

--- commands/tickle.js
+++ commands/tickle.js
@@ -1,17 +1,10 @@
-const { EmbedBuilder } = require('discord.js');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
+const { runAction } = require('../utils/actionHandler.js');
 module.exports = {
-    name: 'tickle',
-    description: '¡Hazle cosquillas a alguien hasta que no pueda más! 😂',
-    async execute(message, args) {
-        const user = message.mentions.users.first();
-        if (!user) return message.reply("🌸 ¿A quién le harás cosquillas hoy? ✨");
-        const res = await fetch('https://nekos.life/api/v2/img/tickle');
-        const json = await res.json();
-        const embed = new EmbedBuilder()
-            .setDescription(`😂 **${message.author.username}** le hace cosquillas a **${user.username}**... ¡Qué risa! 🎀`)
-            .setImage(json.url).setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'tickle', description: 'Hazle cosquillas a alguien', category: 'interacción',
+    async execute(input) {
+        const target = input.mentions.users.first();
+        if (!target) return input.reply({ content: "❌ Las sombras exigen que menciones a un objetivo.", ephemeral: true });
+        const result = await runAction(input, 'tickle', target);
+        await input.reply(result);
     }
 };
