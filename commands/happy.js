8d6039a50b80174b121e5c08fad86dfae3ea7d76
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:16:56 -0300

    Update happy.js

--- commands/happy.js
+++ commands/happy.js
@@ -1,21 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'happy',
-    description: 'Muestra lo feliz que estás hoy ✨',
-    async execute(message) {
-        // Usando un link directo aesthetic si no quieres depender de APIs externas para todo
-        const gifs = [
-            "https://media.tenor.com/S96p6K3mY6AAAAAC/anime-happy.gif",
-            "https://media.tenor.com/0uB0C39U7mAAAAAC/anime-girl-happy.gif"
-        ];
-        const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
-
-        const embed = new EmbedBuilder()
-            .setDescription(`✨ **${message.author.username}** está irradiando felicidad hoy... ¡Qué linda! 🌸`)
-            .setImage(randomGif)
-            .setColor('#FFB7C5');
-
-        await message.reply({ embeds: [embed] });
+    name: 'happy', description: 'Estás feliz ✨', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'happy');
+        await input.reply(result);
     }
 };
