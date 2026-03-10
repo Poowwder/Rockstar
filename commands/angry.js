17c077cf623ebc658a50c3f3724feb9c40f56e9c
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:03:38 -0300

    Update angry.js

--- commands/angry.js
+++ commands/angry.js
@@ -1,12 +1,11 @@
-const { EmbedBuilder } = require('discord.js');
+const { runReaction } = require('../utils/reactionHandler.js');
 
 module.exports = {
     name: 'angry',
-    description: 'Muestra que estás muy enojada 💢',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setDescription(`💢 **${message.author.username}** está muy molesta... ¡Mejor dásle su espacio! ✨`)
-            .setImage("https://media.tenor.com/p_pU8-M79XgAAAAC/anime-angry.gif").setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    description: 'Arde de furia 💢',
+    category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'angry');
+        await input.reply(result);
     }
 };
