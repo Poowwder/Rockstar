2ba77e549127e3459b76a4eb661f1c77afb73b97
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:17:24 -0300

    Update hi.js

--- commands/hi.js
+++ commands/hi.js
@@ -1,14 +1,8 @@
-const { EmbedBuilder } = require('discord.js');
-
+const { runReaction } = require('../utils/reactionHandler.js');
 module.exports = {
-    name: 'hi',
-    aliases: ['hello', 'hola'],
-    description: '¡Saluda a todos con mucha energía! ✨',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setDescription(`✨ **${message.author.username}** dice: ¡Hola a todos, personitas lindas! 🌸`)
-            .setImage("https://media.tenor.com/7P7S9X9v_YAAAAC/anime-wave.gif")
-            .setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    name: 'hi', description: 'Dí hola! 👋', category: 'reacción',
+    async execute(input) {
+        const result = await runReaction(input, 'hi');
+        await input.reply(result);
     }
 };
