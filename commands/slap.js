828e763279bc9bdd45900dff1b2b599d5d39a7b3
Poowwder <ibettyboopp@gmail.com>
2026-03-10 20:01:00 -0300

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -1,17 +1,14 @@
-const { EmbedBuilder } = require('discord.js');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const { runAction } = require('../utils/actionHandler.js');
 
 module.exports = {
     name: 'slap',
-    description: 'Dale una cachetada a alguien 💢',
-    async execute(message, args) {
-        const user = message.mentions.users.first();
-        if (!user) return message.reply("🌸 Tienes que mencionar a alguien para darle un correctivo, linda. ✨");
-        const res = await fetch('https://nekos.life/api/v2/img/slap');
-        const json = await res.json();
-        const embed = new EmbedBuilder()
-            .setDescription(`💢 **${message.author.username}** le dio una cachetada a **${user.username}**... ¡Eso dolió! 🎀`)
-            .setImage(json.url).setColor('#FFB7C5');
-        await message.reply({ embeds: [embed] });
+    description: 'Dale una cachetada a alguien 🖐️',
+    category: 'interacción',
+    async execute(input) {
+        const target = input.mentions.users.first();
+        if (!target) return input.reply({ content: "❌ Las sombras exigen que menciones a un objetivo.", ephemeral: true });
+        
+        const result = await runAction(input, 'slap', target);
+        await input.reply(result);
     }
 };
