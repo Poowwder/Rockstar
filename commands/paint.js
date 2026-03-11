306a7ee05f4c48c45731a1f26d5d768cd55f8130
Poowwder <ibettyboopp@gmail.com>
2026-03-11 02:57:55 -0300

    Update paint.js

--- commands/paint.js
+++ commands/paint.js
@@ -1,6 +1,6 @@
 const { runReaction } = require('../utils/acctionHandler.js');
 module.exports = {
-    name: 'paint', description: 'Le pinto la cara a 🖌️', category: 'interaction',
+    name: 'paint', description: 'Le pinto la cara a 🖌️', category: 'interacción',
     async execute(input) {
         const result = await runReaction(input, 'paint');
         await input.reply(result);
