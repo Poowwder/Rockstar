90f4e519ece950971c714239b402ad6c44002827
Poowwder <ibettyboopp@gmail.com>
2026-03-11 03:20:11 -0300

    Update paint.js

--- commands/paint.js
+++ commands/paint.js
@@ -1,8 +1,11 @@
-const { runReaction } = require('../utils/acctionHandler.js');
+const { runAction } = require('../utils/actionHandler.js');
 module.exports = {
-    name: 'paint', description: 'Le pinto la cara a 🖌️', category: 'interacción',
+    name: 'paint', description: 'Pinta un retrato de alguien', category: 'interacción',
     async execute(input) {
-        const result = await runReaction(input, 'paint');
+        const target = input.mentions.users.first();
+        if (!target) return input.reply({ content: "❌ Las sombras exigen que menciones a un objetivo.", ephemeral: true });
+        
+        const result = await runAction(input, 'paint', target);
         await input.reply(result);
     }
 };
